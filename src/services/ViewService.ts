53cf1fa91fba641334e1dbbf5f2c5a5a07f4636a
PTIT-NoTarget <HanND.B21CN324@stu.ptit.edu.vn>
2024-11-07 23:36:47 +0700

    auth

--- src/services/ViewService.ts
+++ src/services/ViewService.ts
@@ -1,5 +1,4 @@
 import api from '@/services/interceptors'
-import axios from 'axios'
 
 export const ViewService = {
   getViewByViewName (viewName: string) {
@@ -7,13 +6,7 @@ export const ViewService = {
   },
 
   getViewByMultiViewName (viewNames: string[]) {
-    // return api.post(`view/multi`, viewNames)
-    return axios.post(`${import.meta.env.VITE_APP_API_BASE_URL}/view/multi`, viewNames,
-      {
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
-        }
-      })
+    return api.post(`view/multi`, viewNames)
   }
 }
 
--- vite.config.mts
+++ vite.config.mts
@@ -70,5 +70,14 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    cors: { origin: "*" },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
   },
 })
